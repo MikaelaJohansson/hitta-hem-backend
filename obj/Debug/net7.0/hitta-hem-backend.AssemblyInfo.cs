d1d3ea0aece5a8a2003ea5f0c9a6dba5bf64d0f4
Mikaela Johansson <mikaela.johansson87@gmail.com>
2025-07-28 16:22:22 +0200

    Bytte till .NET 7 och rätt OpenAPI-version

--- obj/Debug/net7.0/hitta-hem-backend.AssemblyInfo.cs
+++ obj/Debug/net7.0/hitta-hem-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hitta-hem-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2261bb0a75e67f5029923cb050b58536e1824185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6444a87d2a0276c41b00cb5a4d201dd59bc2708e")]
 [assembly: System.Reflection.AssemblyProductAttribute("hitta-hem-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hitta-hem-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
